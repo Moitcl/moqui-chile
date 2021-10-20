99c505f078e5d906792cbc0c58c5c25c2ee40943
Jens Hardings <jhp@moit.cl>
2021-10-20 13:38:43 -0300

    feat: add error description in enviaSII#Documento

--- service/mchile/DTEServices/enviaSiiDocumento.groovy
+++ service/mchile/DTEServices/enviaSiiDocumento.groovy
@@ -51,5 +51,7 @@ if(statusXML.equals("0")) {
     trackId = trackId.replaceAll("siid:TRACKID>","")
     ec.logger.warn("DTE Enviada correctamente con trackId " + trackId)
 } else {
-    ec.message.addMessage("Error "+ statusXML + " al enviar DTE", "danger")
+    errorDescriptionMap = ['0':'Upload OK', '1':'El Sender no tiene permiso para enviar', '2':'Error en tamaño del archivo (muy grande o muy chico)', '3':'Archivo cortado (tamaño != al parámetro size)',
+                           '5':'No está auten†icado', '6':'Empresa no autorizada a enviar archivos', '7':'Esquema Inválido', '8':'Firma del Documento', '9':'Sistema Bloqueado', '0':'Error Interno']
+    ec.message.addMessage("Error "+ statusXML + " al enviar DTE (${errorDescriptionMap[statusXML]})", "danger")
 }
