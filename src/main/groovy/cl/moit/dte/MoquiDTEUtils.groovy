c076d7a5854be4cddcfd03dc8dd31a3e7b904553
Jens Hardings <jhp@moit.cl>
2021-12-05 21:52:55 -0300

    feat: move services related to DTE communication to DTECommServices, advance in processing incoming email incl validating signatures

--- src/main/groovy/cl/moit/dte/MoquiDTEUtils.groovy
+++ src/main/groovy/cl/moit/dte/MoquiDTEUtils.groovy
@@ -4,13 +4,45 @@ import cl.nic.dte.util.Utilities
 import cl.sii.siiDte.DTEDefType.Documento.Detalle
 import cl.sii.siiDte.DTEDefType.Documento.Referencia
 import cl.sii.siiDte.FechaType
+import org.apache.xml.security.exceptions.XMLSecurityException
 import org.moqui.BaseArtifactException
 import org.moqui.context.ExecutionContext
 import org.moqui.entity.EntityValue
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.w3c.dom.Element
+import org.w3c.dom.Document
+import org.w3c.dom.NodeList
+import org.xml.sax.SAXException
 
+import javax.xml.crypto.AlgorithmMethod
+import javax.xml.crypto.KeySelector
+import javax.xml.crypto.KeySelectorException
+import javax.xml.crypto.KeySelectorResult
+import javax.xml.crypto.XMLCryptoContext
+import javax.xml.crypto.XMLStructure
+import javax.xml.crypto.dsig.SignatureMethod
+import javax.xml.crypto.dsig.XMLSignatureFactory
+import javax.xml.crypto.dsig.dom.DOMValidateContext
+import javax.xml.crypto.dsig.keyinfo.KeyInfo
+import javax.xml.crypto.dsig.keyinfo.X509Data
+import javax.xml.namespace.NamespaceContext
+import javax.xml.parsers.ParserConfigurationException
+import javax.xml.xpath.XPath
+import javax.xml.xpath.XPathFactory
+import javax.xml.xpath.XPathExpression
+import javax.xml.xpath.XPathConstants
+import java.security.InvalidKeyException
+import java.security.Key
+import java.security.NoSuchAlgorithmException
+import java.security.PublicKey
+import java.security.cert.X509Certificate
 import java.sql.Timestamp
+import javax.xml.crypto.dsig.XMLSignature
 
 class MoquiDTEUtils {
+
+    protected final static Logger logger = LoggerFactory.getLogger(MoquiDTEUtils.class)
 
     public static HashMap<String, Object> prepareDetails(ExecutionContext ec, List<HashMap> detailList, String detailType) throws BaseArtifactException {
         return prepareDetails(ec, detailList, detailType, null)
@@ -205,6 +237,115 @@ class MoquiDTEUtils {
             i = i + 1
         }
         return [referenceArray:ref, anulaBoleta:anulaBoleta, folioAnulaBoleta:folioAnulaBoleta, dteExenta:dteExenta]
+    }
+
+    public static boolean verifySignature(Document doc, String xPathExpression) throws NoSuchAlgorithmException, InvalidKeyException,
+            IOException, ParserConfigurationException, SAXException, XMLSecurityException {
+        XPath xpath = XPathFactory.newInstance().newXPath()
+        xpath.setNamespaceContext(new DefaultNamespaceContext().addNamespace("sii", "http://www.sii.cl/SiiDte"))
+        XPathExpression expression = xpath.compile(xPathExpression)
+        NodeList nodes = (NodeList) expression.evaluate(doc.getDocumentElement(), XPathConstants.NODESET)
+        if (nodes == null || nodes.length < 1)
+            throw new RuntimeException("Could not find any node using XPath expression ${xPathExpression}")
+        int verificationCount = 0
+
+        nodes.each { org.w3c.dom.Node node ->
+            verificationCount++
+            ((Element)node).setIdAttributeNS(null, "ID", true)
+            NodeList signatureNodeList = ((Element)node.getParentNode()).getElementsByTagNameNS("http://www.w3.org/2000/09/xmldsig#", "Signature");
+            if (signatureNodeList == null || signatureNodeList.length < 1)
+                throw (new SAXException("No se encuentra firma para verificacion ${verificationCount}"));
+            org.w3c.dom.Element signatureElem = (org.w3c.dom.Element) signatureNodeList.item(signatureNodeList.length - 1);
+            if (signatureElem == null) {
+                throw (new SAXException("No se encuentra firma para verificacion ${verificationCount}"));
+            }
+            DOMValidateContext valContext = new DOMValidateContext(new X509KeySelector(), signatureElem)
+
+// Unmarshal the XMLSignature.
+            XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM");
+            XMLSignature signature = fac.unmarshalXMLSignature(valContext);
+
+// Validate the XMLSignature.
+            if (!signature.validate(valContext))
+                return false
+        }
+        logger.info("Checked ${verificationCount} signatures successfully")
+        return true;
+    }
+
+    public static class DefaultNamespaceContext implements NamespaceContext {
+        private HashMap<String,String> keyMap = new HashMap<String,String>()
+
+        public DefaultNamespaceContext addNamespace(String prefix, String uri) {
+            keyMap.put(prefix, uri)
+            return this
+        }
+
+        @Override
+        String getNamespaceURI(String prefix) {
+            return keyMap.get(prefix)
+        }
+
+        @Override
+        String getPrefix(String namespaceURI) {
+            keyMap.keySet().each { key, value ->
+                if (value == namespaceURI)
+                    return key
+            }
+            return null
+        }
+
+        @Override
+        Iterator<String> getPrefixes(String namespaceURI) {
+            LinkedList<String> prefixList = new LinkedList<String>()
+            keyMap.keySet().each { key, value ->
+                if (value == namespaceURI)
+                    prefixList.add(key)
+            }
+            return prefixList
+        }
+    }
+
+    public static class X509KeySelector extends KeySelector {
+        public KeySelectorResult select(KeyInfo keyInfo,
+                                        KeySelector.Purpose purpose,
+                                        AlgorithmMethod method,
+                                        XMLCryptoContext context)
+                throws KeySelectorException {
+            Iterator ki = keyInfo.getContent().iterator();
+            while (ki.hasNext()) {
+                XMLStructure info = (XMLStructure) ki.next();
+                if (!(info instanceof X509Data))
+                    continue;
+                X509Data x509Data = (X509Data) info;
+                Iterator xi = x509Data.getContent().iterator();
+                while (xi.hasNext()) {
+                    Object o = xi.next();
+                    if (!(o instanceof X509Certificate))
+                        continue;
+                    final PublicKey key = ((X509Certificate)o).getPublicKey();
+                    // Make sure the algorithm is compatible
+                    // with the method.
+                    if (algEquals(method.getAlgorithm(), key.getAlgorithm())) {
+                        return new KeySelectorResult() {
+                            public Key getKey() { return key; }
+                        };
+                    }
+                }
+            }
+            throw new KeySelectorException("No key found!");
+        }
+
+        static boolean algEquals(String algURI, String algName) {
+            if ((algName.equalsIgnoreCase("DSA") &&
+                    algURI.equalsIgnoreCase(SignatureMethod.DSA_SHA1)) ||
+                    (algName.equalsIgnoreCase("RSA") &&
+                            algURI.equalsIgnoreCase(SignatureMethod.RSA_SHA1))) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
 }
