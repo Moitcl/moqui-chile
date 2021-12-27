e029a18d7131a36951535b32a6dae9d6ca0a0af0
Jens Hardings <jhp@moit.cl>
2021-12-27 14:05:02 -0300

    fix: in generaFactura, limit IT1 in TED to 40 chars considering shorter items as well

--- service/mchile/sii/DTEServices/generaFactura.groovy
+++ service/mchile/sii/DTEServices/generaFactura.groovy
@@ -220,7 +220,10 @@ if (giroReceptor.length() > 39)
     giroReceptor = giroReceptor.substring(0,39)
 
 // Timbre
-datosTed = "<DD><RE>${rutEmisor}</RE><TD>${tipoDte}</TD><F>${folio}</F><FE>${ec.l10n.format(fechaEmision, "yyyy-MM-dd")}</FE><RR>${rutReceptor}</RR><RSR>${razonSocialReceptor}</RSR><MNT>${totalInvoice}</MNT><IT1>${detalleList.get(0).nombreItem}</IT1>${folioResult.cafFragment.replaceAll('>\\s*<', '><').trim()}<TSTED>${ec.l10n.format(ec.user.nowTimestamp, "yyyy-MM-dd'T'HH:mm:ss")}</TSTED></DD>"
+String detalleIt1 = detalleList.get(0).nombreItem
+if (detalleIt1.length() > 40)
+    detalleIt1 = detalleIt1.substring(0, 40)
+datosTed = "<DD><RE>${rutEmisor}</RE><TD>${tipoDte}</TD><F>${folio}</F><FE>${ec.l10n.format(fechaEmision, "yyyy-MM-dd")}</FE><RR>${rutReceptor}</RR><RSR>${razonSocialReceptor}</RSR><MNT>${totalInvoice}</MNT><IT1>${detalleIt1}</IT1>${folioResult.cafFragment.replaceAll('>\\s*<', '><').trim()}<TSTED>${ec.l10n.format(ec.user.nowTimestamp, "yyyy-MM-dd'T'HH:mm:ss")}</TSTED></DD>"
 
 xmlBuilder.DTE(xmlns: 'http://www.sii.cl/SiiDte', 'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance', version: '1.0', 'xsi:schemaLocation': 'http://www.sii.cl/SiiDte DTE_v10.xsd') {
     Documento(ID: idDocumento) {
