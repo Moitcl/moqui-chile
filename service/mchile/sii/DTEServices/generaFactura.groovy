8a2d5a5bf49d45013c5cf5a4c23a9433a7c34172
Jens Hardings <jhp@moit.cl>
2022-07-07 12:09:46 -0400

    hotfix: do not create GuiaDespacho

--- service/mchile/sii/DTEServices/generaFactura.groovy
+++ service/mchile/sii/DTEServices/generaFactura.groovy
@@ -493,9 +493,11 @@ Timestamp ts = new Timestamp(date.getTime())
 dteEv.date = ts
 dteEv.update()
 
+/*
 if (tipoDte == 52) {
     ec.service.sync().name("store#mchile.dte.GuiaDespacho").parameters([fiscalTaxDocumentId:dteEv.fiscalTaxDocumentId, indTrasladoEnumId:indTrasladoEnumId]).call()
 }
+ */
 
 xmlContentLocation = "dbresource://moit/erp/dte/${rutEmisor}/DTE-${tipoDte}-${folio}.xml"
 pdfContentLocation = "dbresource://moit/erp/dte/${rutEmisor}/DTE-${tipoDte}-${folio}.pdf"
