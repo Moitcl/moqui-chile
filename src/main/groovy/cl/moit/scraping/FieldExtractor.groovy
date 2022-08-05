907cb140d0b3a6201eea5ea58a074bc539247b58
Jens Hardings <jhp@moit.cl>
2022-08-05 09:00:08 -0400

    refactor: add basic documentation to FieldExtractor

--- src/main/groovy/cl/moit/scraping/FieldExtractor.groovy
+++ src/main/groovy/cl/moit/scraping/FieldExtractor.groovy
@@ -1,6 +1,5 @@
 package cl.moit.scraping
 
-
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -11,7 +10,20 @@ class FieldExtractor {
 
     private static final Logger logger = LoggerFactory.getLogger(FieldExtractor.class);
 
-    public static Map<String,String> extractFromPatternList(String text, List<String> patternList) {
+    /**
+     *
+     * @param text text to be scraped
+     * @param patternList list of patterns to search within the @text. Each pattern is a Map<String,String> that contains following fields:
+     *        name: field name to use in the return value for matched patterns
+     *        preselectPattern: an initial filter so that the pattern (see below) is searched for in the matched text of this pattern, as
+     *                          defined by the capturing group (see java.util.regex.Pattern)
+     *        pattern: regex to find the match and return the desired value as a capturing group (see java.util.regex.Pattern). If the pattern is
+     *                 found once within the text (pre-filtered by preselectPattern if present), the value is the String, otherwise it is a
+     *                 List<String>.
+     * @return a map of strings with the field name as key and the text indicated in the pattern (either a String or a List<Sting> if the pattern
+     *         is matched multiple times) as the value
+     */
+    public static Map<String,Object> extractFromPatternList(String text, List<Map<String,String>> patternList) {
         Map<String,String> campos = [:]
         patternList.each {
             //println("attempting pattern ${it.pattern} of type ${it.pattern.class}")
@@ -44,7 +56,7 @@ class FieldExtractor {
                 } else
                     campoList.add(match[1].trim())
             }
-            campos.put(it['name'], campoList.size() == 1? campoList.get(0): campoList)
+            campos.put(it.name, campoList.size() == 1? campoList.get(0): campoList)
         }
         return campos
     }
